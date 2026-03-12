ce2385bdadbfd6ee4545ae73a8259a473166c687
theboyfisco <ifeanyodigili@gmail.com>
2026-03-12 15:29:40 +0100

    iiiif

--- testLog.js
+++
@@ -1,5 +0,0 @@
-console.log('--- Test JS Log Script Running ---');
-const a = 10;
-const b = 20;
-console.log(`JS Calculation: ${a} + ${b} = ${a + b}`);
-console.log('--- Test JS Log Script Finished ---');
--- testLog.ts
+++
@@ -1,5 +0,0 @@
-console.log('--- Test Log Script Running ---');
-const a = 1;
-const b = 2;
-console.log(`Calculation: ${a} + ${b} = ${a + b}`);
-console.log('--- Test Log Script Finished ---');
